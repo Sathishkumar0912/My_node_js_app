9caf461daad08ae9f4d70edc4500e03fd18a99ef
Sathishkumar_0912 <sathishkumarvvps9360385503@gmail.com>
2025-07-09 11:26:01 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = 2000;
+
+// Serve static files from the "public" directory
+//app.use(express.static('public'));
+
+// Route to serve the HTML file
+app.get('/sathishkumar', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+app.get('/', (req, res) => {
+  res.write("Hello Sathishkumar");
+  res.end();
+});
+/*
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
+})*/
+
+app.listen(2000)
