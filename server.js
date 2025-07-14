3a6510a88143fcf2a964e64ed334ecf9ff36f913
SATHISHKUMAR V <151901974+Sathishkumar0912@users.noreply.github.com>
2025-07-14 18:31:02 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const PORT = 2000;
+const port = process.env.PORT || 8080;
 
 // Serve static files from the "public" directory
 //app.use(express.static('public'));
